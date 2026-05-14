ff5a1af41d66b1277408d59b6db115dba0015a7a
zcastro6-web <zcastro6@horizon.csueastbay.edu>
2026-05-13 20:01:33 -0700

    Update plscript.js

--- plscript.js
+++ plscript.js
@@ -3,63 +3,63 @@ const landscapeGallery = document.getElementById("landscapeGallery");
 const landscapes = [
 
 {
-  img: "LongPhoto/Photo1.jpg",
+  img: "Photo1.jpg",
   title: "Snow",
   desc: "Yosemite in winter",
   date: "March 14, 2025",
 },
 
 {
-  img: "LongPhoto/Photo2.jpg",
+  img: "Photo2.jpg",
   title: "Obscure",
   desc: "Yosemite in winter",
   date: "March 14, 2025",
 },
 
 {
-  img: "LongPhoto/Photo3.jpg",
+  img: "Photo3.jpg",
   title: "Life Goes On",
   desc: "Commute",
   date: "January 31, 2024",
 },
 
 {
-  img: "LongPhoto/Photo4.jpg",
+  img: "Photo4.jpg",
   title: "Hazy",
   desc: "Yosemite in winter",
   date: "March 15, 2025",
 },
 
 {
-  img: "LongPhoto/Photo5.jpg",
+  img: "Photo5.jpg",
   title: "Dome",
   desc: "Yosemite in winter",
   date: "January 29, 2024",
 },
 
 {
-  img: "LongPhoto/Photo6.jpg",
+  img: "Photo6.jpg",
   title: "Bridge",
   desc: "Yosemite in winter",
   date: "March 14, 2025",
 },
 
 {
-  img: "LongPhoto/Photo7.jpg",
+  img: "Photo7.jpg",
   title: "White Winter Hymnal",
   desc: "Yosemite in winter",
   date: "March 14, 2025",
 },
 
 {
-  img: "LongPhoto/Photo8.jpg",
+  img: "Photo8.jpg",
   title: "Division",
   desc: "Yosemite in winter",
   date: "March 14, 2025",
 },
 
 {
-  img: "LongPhoto/Photo9.jpg",
+  img: "Photo9.jpg",
   title: "Mountain",
   desc: "Yosemite in winter",
   date: "March 15, 2025",
@@ -73,315 +73,315 @@ const landscapes = [
 },
 
 {
-  img: "LongPhoto/Photo11.jpg",
+  img: "Photo11.jpg",
   title: "Mountain",
   desc: "Yosemite in winter",
   date: "March 15, 2025",
 },
 
 {
-  img: "LongPhoto/Photo12.jpg",
+  img: "Photo12.jpg",
   title: "Mountain",
   desc: "Yosemite in winter",
   date: "March 15, 2025",
 },
 
 {
-  img: "LongPhoto/Photo13.jpg",
+  img: "Photo13.jpg",
   title: "Bay",
   desc: "San Francisco",
   date: "August 12, 2025",
 },
 
 {
-  img: "LongPhoto/Photo14.jpg",
+  img: "Photo14.jpg",
   title: "Joy in the Morning",
   desc: "Jenness Park",
   date: "July 25, 2023",
 },
 
 {
-  img: "LongPhoto/Photo15.jpg",
+  img: "Photo15.jpg",
   title: "Griffith Observatory",
   desc: "The Lord said to Abram after Lot had parted from him, “Look around from where you are, to the north and south, to the east and west. All the land that you see I will give to you and your offspring forever. <br> - Genesis 13:14-15",
   date: "December 28, 2024",
 },
 
 {
-  img: "LongPhoto/Photo16.jpg",
+  img: "Photo16.jpg",
   title: "Flow",
   desc: "Jenness Park",
   date: "July 27, 2023",
 },
 
 {
-  img: "LongPhoto/Photo17.jpg",
+  img: "Photo17.jpg",
   title: "Still",
   desc: "He says, “Be still, and know that I am God; I will be exalted among the nations, I will be exalted in the earth. <br> - Psalm 46:10",
   date: "July 25, 2024",
 },
 
 {
-  img: "LongPhoto/Photo18.jpg",
+  img: "Photo18.jpg",
   title: "Silent River",
   desc: "Jenness Park",
   date: "July 25, 2024",
 },
 
 {
-  img: "LongPhoto/Photo19.jpg",
+  img: "Photo19.jpg",
   title: "Sunlight",
   desc: "CSUEB",
   date: "February 28, 2024",
 },
 
 {
-  img: "LongPhoto/Photo20.jpg",
+  img: "Photo20.jpg",
   title: "Reflection",
   desc: "Jenness Park",
   date: "July 27, 2023",
 },
 
 {
-  img: "LongPhoto/Photo21.jpg",
+  img: "Photo21.jpg",
   title: "Quiet",
   desc: "San Francisco",
   date: "May 17, 2024",
 },
 
 {
-  img: "LongPhoto/Photo22.jpg",
+  img: "Photo22.jpg",
   title: "Bridge",
   desc: "San Francisco",
   date: "April 6, 2026", 
 },
 
 {
-  img: "LongPhoto/Photo23.jpg",
+  img: "Photo23.jpg",
   title: "Sunset",
   desc: "CSUEB",
   date: "April 7, 2025",
 },
 
 {
-  img: "LongPhoto/Photo24.jpg",
+  img: "Photo24.jpg",
   title: "A Glimpse of Freedom",
   desc: "CSUEB",
   date: "February 26, 2024",
 },
 
 {
-  img: "LongPhoto/Photo25.jpg",
+  img: "Photo25.jpg",
   title: "Dining Room",
   desc: "Family trip at Oakhurst.",
   date: "July 6, 2025",
 },
 
 {
-  img: "LongPhoto/Photo26.jpg",
+  img: "Photo26.jpg",
   title: "Living Room",
   desc: "Family trip at Oakhurst.",
   date: "July 6, 2025",
 },
 
 {
-  img: "LongPhoto/Photo27.jpg",
+  img: "Photo27.jpg",
   title: "Living Room",
   desc: "Family trip at Oakhurst.",
   date: "July 6, 2025",
 },
 
 {
-  img: "LongPhoto/Photo28.jpg",
+  img: "Photo28.jpg",
   title: "Luxury",
   desc: "Valley Fair",
   date: "March 9, 2024",
 },
 
 {
-  img: "LongPhoto/Photo29.jpg",
+  img: "Photo29.jpg",
   title: "Beach",
   desc: "Santa Cruz",
   date: "May 31, 2025",
 },
 
 {
-  img: "LongPhoto/Photo30.jpg",
+  img: "Photo30.jpg",
   title: "Warm lighting",
   desc: "Sunset at Milpitas",
   date: "March 1, 2026",
 },
 
 {
-  img: "LongPhoto/Photo31.jpg",
+  img: "Photo31.jpg",
   title: "Warm lighting",
   desc: "Sunset at Milpitas",
   date: "April 5, 2026",
 },
 
 {
-  img: "LongPhoto/Photo32.jpg",
+  img: "Photo32.jpg",
   title: "Warm lighting",
   desc: "Sunset at Milpitas",
   date: "December 16, 2025",
 },
 
 {
-  img: "LongPhoto/Photo33.jpg",
+  img: "Photo33.jpg",
   title: "Warm lighting",
   desc: "Sunset at Milpitas",
   date: "September 18, 2024",
 },
 
 {
-  img: "LongPhoto/Photo34.jpg",
+  img: "Photo34.jpg",
   title: "Cotton Candy",
   desc: "Sunset at San Jose",
   date: "January 17, 2026",
 },
 
 {
-  img: "LongPhoto/Photo35.jpg",
+  img: "Photo35.jpg",
   title: "Open Skies",
   desc: "Wide horizon",
   date: "December 28, 2024",
 },
 
 {
-  img: "LongPhoto/Photo36.jpg",
+  img: "Photo36.jpg",
   title: "Golden Time",
   desc: "Sunset in the Philippines",
   date: "December 31, 2022",
 },
 
 {
-  img: "LongPhoto/Photo37.jpg",
+  img: "Photo37.jpg",
   title: "Purple Horizon",
   desc: "Evening colors",
   date: "March 26, 2025",
 },
 
 {
-  img: "LongPhoto/Photo38.jpg",
+  img: "Photo38.jpg",
   title: "Lavender",
   desc: "Oracle Park",
   date: "April 18, 2024",
 },
 
 {
-  img: "LongPhoto/Photo39.jpg",
+  img: "Photo39.jpg",
   title: "Motion blur",
   desc: "Window shot",
   date: "June 16, 2024",
 },
 
 {
-  img: "LongPhoto/Photo40.jpg",
+  img: "Photo40.jpg",
   title: "Peaceful scenery",
   desc: "Sunset in Milpitas",
   date: "October 21, 2025",
 },
 
 {
-  img: "LongPhoto/Photo41.jpg",
+  img: "Photo41.jpg",
   title: "Downturn",
   desc: "Sunset in Hayward",
   date: "December 4, 2023",
 },
 
 {
-  img: "LongPhoto/Photo42.jpg",
+  img: "Photo42.jpg",
   title: "Humble",
   desc: "Oracle Park",
   date: "April 18, 2024",
 },
 
 {
-  img: "LongPhoto/Photo43.jpg",
+  img: "Photo43.jpg",
   title: "Guidance",
   desc: "Family trip at Oakhurst.",
   date: "July 6, 2025",
 },
 
 {
-  img: "LongPhoto/Photo44.jpg",
+  img: "Photo44.jpg",
   title: "Twilight",
   desc: "Momentary darkness",
   date: "December 28, 2022",
 },
 
 {
-  img: "LongPhoto/Photo45.jpg",
+  img: "Photo45.jpg",
   title: "Campus at Night",
   desc: "Night photography",
   date: "August 31, 2023",
 },
 
 {
-  img: "LongPhoto/Photo46.jpg",
+  img: "Photo46.jpg",
   title: "Stationary",
   desc: "Parking lot in Milpitas",
   date: "January 31, 2024",
 },
 
 {
-  img: "LongPhoto/Photo47.jpg",
+  img: "Photo47.jpg",
   title: "Downpour",
   desc: "Shot from inside my sister's car.",
   date: "January 31, 2024",
 },
 
 {
-  img: "LongPhoto/Photo48.jpg",
+  img: "Photo48.jpg",
   title: "Art in motion",
   desc: "My sister helped me for my assignment.",
   date: "February 2, 2024",
 },
 
 {
-  img: "LongPhoto/Photo49.jpg",
+  img: "Photo49.jpg",
   title: "Origin",
   desc: "My lovely parents.",
   date: "February 2, 2024",
 },
 
 {
-  img: "LongPhoto/Photo50.jpg",
+  img: "Photo50.jpg",
   title: "Smoke &",
   desc: "Danger",
   date: "March 9, 2024",
 },
 
 {
-  img: "LongPhoto/Photo51.jpg",
+  img: "Photo51.jpg",
   title: "Mirrors",
   desc: "Vision",
   date: "March 9, 2024",
 },
 
 {
-  img: "LongPhoto/Photo52.jpg",
+  img: "Photo52.jpg",
   title: "Poetry in Motion",
   desc: "Multiple frames",
   date: "February 14, 2024",
 },
 
 {
-  img: "LongPhoto/Photo53.jpg",
+  img: "Photo53.jpg",
   title: "Writings on the Wall",
   desc: "Vandalism",
   date: "March 18, 2024",
 },
 
 {
-  img: "LongPhoto/Photo54.jpg",
+  img: "Photo54.jpg",
   title: "Vision",
   desc: "Glasses",
   date: "January 17, 2024",
 },
 
 {
-  img: "LongPhoto/Photo55.jpg",
+  img: "Photo55.jpg",
   title: "Spotlight",
   desc: "An ounce of sunlight",
   date: "February 26, 2024",
